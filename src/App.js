06ca843cb50764804c9563c75f2a47c6a5b4bfe3
richard Hu <yongjinhu1230@gmail.com>
2022-06-06 16:27:15 +0800

    add_Outlet_嵌套路由_03

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const Shop = () => {
    return (
      <Routes>
      <Route path='/' element={<Navigation />} >
-       <Route path='home'  element={<Home />} />
+       <Route index  element={<Home />} />
          <Route path='shop' element={<Shop />} />
 
        </Route>
@@ -31,7 +31,11 @@ const Shop = () => {
   );
 }
 
-// 这里是嵌套路由的写法:shop前面没有/,home前面也没有/,这两个是要嵌套在Navigation组件里面
-//最外层是嵌套其他组件的,这里最外层为Navigation,配合Outlet,/home; /shop皆可以显示
+// 这里是嵌套路由的写法:shop前面没有/,这是要嵌套在Navigation组件里面
+//最外层是嵌套其他组件的,这里最外层为Navigation,配合Outlet, /shop皆可以显示
+// <Route index  element={<Home />} />          
+// <Route path="/home"  element={<Home />} />   //这行和上面这行效果等价,只不过原来 /home显示的效果,现在 / 即可
+
+
 
 export default App;
