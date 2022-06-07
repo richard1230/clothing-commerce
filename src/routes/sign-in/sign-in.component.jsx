3b517617943adddd2a508a90bfaea636d87b4542
richard Hu <yongjinhu1230@gmail.com>
2022-06-07 18:54:47 +0800

    delete-redirect-method

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,10 +1,7 @@
-import { useEffect } from 'react';
-import { getRedirectResult } from 'firebase/auth'
+
 import {
-    auth,
     signInWithGooglePopup,
     createUserDocumentFromAuth,
-    signInWithGoogleRedirect
 } from '../../utils/firebase/firebase.utils';
 
 
@@ -15,18 +12,13 @@ const SignIn = () => {
         const userDocRef = await createUserDocumentFromAuth(user);
     }
 
-    useEffect(async () => {
-        const response = await getRedirectResult(auth);
-        if (response) {
-            const userDocRef = await createUserDocumentFromAuth(response.user)
-        }
-    }, [])
+
 
     return (
         <div>
             <h1> Sign In Page</h1>
             <button onClick={logGoogleUser}> Sign in with Google Popup</button>
-            <button onClick={signInWithGoogleRedirect}> Sign in with Google Redir</button>
+
         </div>
     )
 }
