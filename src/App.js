7fd570e91c3be4ee4f9913728d6e8779fb7b52ed
richard Hu <yongjinhu1230@gmail.com>
2022-06-18 22:23:14 +0800

    lesson-111

--- src/App.js
+++ src/App.js
@@ -4,15 +4,11 @@ import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import { UserContext } from "./contexts/user.context";
+import Shop from './routes/shop/shop.component';
 
 
 
 
-const Shop = () => {
-  return (
-    <h1>I am the shop page</h1>
-  )
-}
 function App() {
   const { currentUser} = useContext(UserContext);
 
