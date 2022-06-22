3b7735a31da67fe1d4f70970aa60461ed8948638
richard Hu <yongjinhu1230@gmail.com>
2022-06-22 21:03:20 +0800

    lesson118

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,12 +1,17 @@
 import './cart-item.styles.scss';
 
-const CartItem = ({ props }) => {
-  const { imageUrl, price, name, quantity } = props.cartItem;
+const CartItem = ({ cartItem }) => {
+  const { imageUrl, price, name, quantity } = cartItem;
 
   return (
     <div className='cart-item-container'>
-          <h2>{name}</h2>
-          <span>{ quantity }</span>
+      <img src={imageUrl} alt={`${name}`} />
+      <div className='item-details'>
+        <span className='name'>{name}</span>
+        <span className='price'>
+          {quantity} x ${price}
+        </span>
+      </div>
     </div>
   );
 };
