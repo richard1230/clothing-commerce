fc0b0fcc2f4d56c4db6c0650530000fcc0e12cc2
richard Hu <yongjinhu1230@gmail.com>
2022-06-23 22:09:40 +0800

    lesson-121-123

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 
 import Shop from './routes/shop/shop.component';
+import Checkout from './routes/checkout/checkout.component';
 
 
 
@@ -15,7 +16,8 @@ function App() {
     <Route path='/' element={<Navigation />}>
     <Route index element={<Home />} />
     <Route path='shop' element={<Shop />} />
-    <Route path='auth' element={<Authentication />} />
+        <Route path='auth' element={<Authentication />} />
+        <Route path='checkout' element={<Checkout />} />
   </Route>
     </Routes>
   );
